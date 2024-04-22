84bfb1f5fce4d6b39977823de8d7421822d55afd
HasanAvshar <hesenavs1@gmail.com>
2024-04-22 23:30:39 +0400

    Add project files.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            button.IsEnabled = !string.IsNullOrWhiteSpace(textBox.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+            listBox.Items.Add(text);
+            textBox.Text = "";
+            DateTime time = DateTime.Now;
+            string timeString = time.ToString("HH:mm:ss");
+            label.Content = timeString;
+        }
+    }
+}
